85af77febcf2daa3f8aede109c3df187c0b1063b
Miguel <miguel.jacinto@cleverti.com>
2015-12-22 15:10:25 +0000

    Adding locations to basic content

--- Context/Object/BasicContent.php
+++ Context/Object/BasicContent.php
@@ -11,6 +11,8 @@ namespace EzSystems\BehatBundle\Context\Object;
 
 use Behat\Gherkin\Node\TableNode;
 use PHPUnit_Framework_Assert as Assertion;
+use eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
 
 /**
  * Sentences for Fields
@@ -56,6 +58,44 @@ trait BasicContent
     {
         $parts = explode( '/', rtrim( $path, '/' ) );
         return end( $parts );
+    }
+
+    /**
+     * @Given I add the :location content as location to the content :content
+     */
+    public function addLocation($location, $content) {
+        $repository = $this->getRepository();
+        $searchService = $repository->getSearchService();
+        $criterion = new Criterion\LogicalOr(
+            array(
+                new Criterion\Field("name", Criterion\Operator::EQ, $location),
+                new Criterion\Field("title", Criterion\Operator::EQ, $location)
+            )
+        );
+        $query = new Query();
+        $query->query = $criterion;
+        $searchResult = $searchService->findContent($query);
+        $folderContent = $searchResult->searchHits[0]->valueObject;
+        $criterion = new Criterion\LogicalOr(
+            array(
+                new Criterion\Field("title", Criterion\Operator::EQ, $content),
+                new Criterion\Field("name", Criterion\Operator::EQ, $content)
+            )
+        );
+        $query->query = $criterion;
+        $searchResult = $searchService->findContent($query);
+        $articleContent = $searchResult->searchHits[0]->valueObject;
+
+        $repository->sudo(
+            function () use ($repository, $folderContent, $articleContent) {
+                $contentService = $repository->getContentService();
+                $locationService = $repository->getLocationService();
+                $folderContentInfo = $contentService->loadContentInfo($folderContent->id);
+                $locationCreateStruct = $locationService->newLocationCreateStruct($folderContentInfo->mainLocationId);
+                $articleContentInfo = $contentService->loadContentInfo($articleContent->id);
+                $newLocation = $locationService->createLocation($articleContentInfo, $locationCreateStruct);
+            }
+        );
     }
 
     /**
